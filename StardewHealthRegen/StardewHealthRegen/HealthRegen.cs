17e1f3978e3d2365cfef9a0ec5f279399c50e0d1
Mario Tacke <tacke.mario@gmail.com>
2016-04-14 21:20:12 -0700

    (feature) Regenerate 1 health every 1 second

---
+++ StardewHealthRegen/StardewHealthRegen/HealthRegen.cs
@@ -0,0 +1,26 @@
+﻿namespace StardewHealthRegen
+{
+    using StardewModdingAPI;
+    using StardewModdingAPI.Events;
+
+    using StardewValley;
+
+    public class HealthRegen : Mod
+    {
+        public override void Entry(params object[] objects)
+        {
+            GameEvents.OneSecondTick += GameEvents_OneSecondTick;
+        }
+
+        private static void GameEvents_OneSecondTick(object sender, System.EventArgs e)
+        {
+            if (Game1.player == null || !Game1.hasLoadedGame)
+                return;
+
+            if (Game1.player.health < Game1.player.maxHealth)
+            {
+                Game1.player.health += 1;
+            }
+        }
+    }
+}
